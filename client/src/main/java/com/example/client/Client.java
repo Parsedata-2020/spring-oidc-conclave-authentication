0c388dbaf9b4bd648489a5609043d986edac2af3
Daniel Shteinbok <dshteinbok@gmail.com>
2021-06-24 16:30:28 -0400

    added okhttp get request to client, simply getting and deserializing the enclave remote attestation

--- client/src/main/java/com/example/client/Client.java
+++ client/src/main/java/com/example/client/Client.java
@@ -1,6 +1,23 @@
 package com.example.client;
 
+import com.r3.conclave.common.EnclaveInstanceInfo;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request attestationRequest = new Request.Builder()
+                .url("http://localhost:8080")
+                .build();
+        Response attestationResponse = client.newCall(attestationRequest).execute();
+        EnclaveInstanceInfo attestation
+                = EnclaveInstanceInfo.deserialize(attestationResponse.body().bytes());
+        System.out.println("Client received attestation!");
+        System.out.println(attestation.toString());
     }
 }
