4a2002077094d906d0e3c2c3036c31c8b0088eb3
Daniel Shteinbok <dshteinbok@gmail.com>
2021-07-08 09:32:12 -0400

    wrote empty functions for some of the necessary integration tests (that I can think of right now

--- host/src/test/java/com/example/host/OidcServerApplicationTests.java
+++ host/src/test/java/com/example/host/OidcServerApplicationTests.java
@@ -10,4 +10,23 @@ class OidcServerApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void authenticatedUserSendingMail() {
+		// TODO what endpoint does the user send to?
+		// TODO is it a GET or POST?
+		// user should successfully send mail to the enclave, need some enclave feedback?
+	}
+
+	@Test
+	public void authenticatedUserPubkeyMismatch() {
+		// same as above, but the Mail should be generated with a different key than in id token
+	}
+
+	@Test
+	public void modifiedTokenShouldFail() {
+		// e.g. host injects its own public key into the token state, should fail at enclave
+		// due to the token not matching its signature
+		// token header should be accessed through oauth2login().tokenEndpoint.accessTokenResponseClient()
+	}
+
 }
