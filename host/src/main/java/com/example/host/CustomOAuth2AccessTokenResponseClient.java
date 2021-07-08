b4566969b839ee7df7d50316ce5d7e02ea8d256d
Daniel Shteinbok <dshteinbok@gmail.com>
2021-07-08 12:43:49 -0400

    Putting AuthenticationWrapper instance into authenticationWrappers Hashmap

--- host/src/main/java/com/example/host/CustomOAuth2AccessTokenResponseClient.java
+++ host/src/main/java/com/example/host/CustomOAuth2AccessTokenResponseClient.java
@@ -24,6 +24,11 @@ public class CustomOAuth2AccessTokenResponseClient implements OAuth2AccessTokenR
     public OAuth2AccessTokenResponse getTokenResponse
             (AbstractOAuth2AuthorizationGrantRequest authorizationGrantRequest) {
         OAuth2AccessTokenResponse response = defaultAuthorizationCodeTokenResponseClient.getTokenResponse(authorizationGrantRequest);
+        // TODO: Would it make sense to abandon AuthenticationWrapper
+        //  and just map Authentication -> OAuth2AccessTokenResponse ?
+        AuthenticationWrapper authenticationWrapper = new AuthenticationWrapper((Authentication) response.getAccessToken(), response.getAdditionalParameters());
+        System.out.println("New AuthenticationWrapper:\n" + authenticationWrapper.toString());
+        authenticationWrappers.put((Authentication) response.getAccessToken(), authenticationWrapper);
         return response;
     }
 }
