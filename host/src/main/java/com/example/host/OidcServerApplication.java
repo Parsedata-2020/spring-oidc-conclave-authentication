2e05ac317a3eec8b380a7cc1d10e5c6e0c73b51f
Daniel Shteinbok <dshteinbok@gmail.com>
2021-07-08 12:11:04 -0400

    Created Hashtable Bean to store AuthenticationWrappers

--- host/src/main/java/com/example/host/OidcServerApplication.java
+++ host/src/main/java/com/example/host/OidcServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.web.servlet.oauth2.client.OAuth2ClientSecurityMarker;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
@@ -15,6 +16,8 @@ import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenRespon
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Hashtable;
 
 @SpringBootApplication
 public class OidcServerApplication {
@@ -31,6 +34,12 @@ public class OidcServerApplication {
 		EnclaveHost enclave = EnclaveHost.load(enclaveName);
 		enclave.start(null, null);
 		return enclave;
+	}
+
+	@Bean
+	public Hashtable<Authentication, AuthenticationWrapper> authenticationWrappers() {
+	    System.out.println("CREATED NEW authenticationWrappers HASHTABLE");
+		return new Hashtable<>();
 	}
 
 	public static void main(String[] args) {
