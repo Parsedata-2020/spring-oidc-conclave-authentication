5e71e3ffa4e486170345867734f2ea43c8b163f8
Daniel Shteinbok <dshteinbok@gmail.com>
2021-07-09 16:36:58 -0400

    set both the secret value and its hash for the nonce of the AuthorizationRequest, fixing invalid_nonce

--- host/src/main/java/com/example/host/CustomizationRequestResolver.java
+++ host/src/main/java/com/example/host/CustomizationRequestResolver.java
@@ -3,9 +3,16 @@ package com.example.host;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
 
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -36,6 +43,15 @@ public class CustomizationRequestResolver
         //System.out.println("additional parameters: " + req.getAdditionalParameters());
         if (req != null) {
             System.out.println("additional parameters: " + req.getAdditionalParameters());
+            System.out.println("attribute nonce: " + req.getAttribute(OidcParameterNames.NONCE));
+            try {
+                System.out.println(checkHashMatches(req)
+                        ? "Hash of " + req.getAttribute(OidcParameterNames.NONCE)
+                            + " is " + req.getAdditionalParameters().get("nonce")
+                        : "Nonce hash doesn't match");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
         }
         return req;
     }
@@ -50,6 +66,29 @@ public class CustomizationRequestResolver
 
     private Consumer<OAuth2AuthorizationRequest.Builder> pubkeyInNonce(String pub_key) {
         return customizer ->
-                customizer.additionalParameters(params -> params.put("nonce", pub_key));
+        {
+            customizer.attributes(params -> params.put(OidcParameterNames.NONCE, pub_key))
+            .additionalParameters(params -> {
+                try {
+                    params.put("nonce", createHash(pub_key));
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            });
+        };
     }
+
+    private boolean checkHashMatches(OAuth2AuthorizationRequest req) throws NoSuchAlgorithmException {
+        String nonceHash = createHash(req.getAttribute(OidcParameterNames.NONCE));
+        String nonceHashClaim = (String) req.getAdditionalParameters().get("nonce");
+        return nonceHash.equals(nonceHashClaim);
+    }
+
+    static String createHash(String nonce) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] digest = md.digest(nonce.getBytes(StandardCharsets.US_ASCII));
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
+    }
+
 }
+
