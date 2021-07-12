4e5c9d1d5c553b11370a4f103b9a818fad5eb98a
Daniel Shteinbok <dshteinbok@gmail.com>
2021-07-12 13:53:45 -0400

    added shard module with request wrapper

--- enclave/src/main/java/com/example/enclave/VerifierEnclave.java
+++ enclave/src/main/java/com/example/enclave/VerifierEnclave.java
@@ -3,4 +3,8 @@ package com.example.enclave;
 import com.r3.conclave.enclave.Enclave;
 
 public class VerifierEnclave extends Enclave {
+    @Override
+    public byte[] receiveFromUntrustedHost(byte[] bytes) {
+        return null;
+    }
 }
---
+++ shared/src/main/java/RequestWrapper.java
@@ -0,0 +1,2 @@
+public class RequestWrapper {
+}
