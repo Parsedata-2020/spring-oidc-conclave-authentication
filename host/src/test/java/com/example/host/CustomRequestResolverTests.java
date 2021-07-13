eceac4b2f0bed8a389f7cfd1f745733a7485235f
Daniel Shteinbok <dshteinbok@gmail.com>
2021-07-13 17:18:03 -0400

    trying to mock ClientRegistration to properly test the RequestResolver

--- host/src/test/java/com/example/host/CustomRequestResolverTests.java
+++ host/src/test/java/com/example/host/CustomRequestResolverTests.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Collections;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CustomRequestResolverTests {
     /*
@@ -31,13 +37,27 @@ public class CustomRequestResolverTests {
     public void setUp() {
         // DOES NOT WORK!!
         repository = mock(ClientRegistrationRepository.class);
+        ClientRegistration mockRegistration = mock(ClientRegistration.class);
+        when(mockRegistration.getRegistrationId()).thenReturn("google");
+        when(mockRegistration.getClientAuthenticationMethod()).thenReturn(ClientAuthenticationMethod.CLIENT_SECRET_JWT);
+        when(mockRegistration.getAuthorizationGrantType()).thenReturn(AuthorizationGrantType.AUTHORIZATION_CODE);
+        when(mockRegistration.getScopes()).thenReturn(Collections.singleton(OidcScopes.OPENID));
+        /*
+        ClientRegistration mockRegistration = (new ClientRegistration.Builder())
+                .registrationId("google")
+                .clientId("900783094046-ofbmg9vjjj3s7rlothspmhar185g72il.apps.googleusercontent.com")
+                .clientName("")
+                .clientSecret("")
+                .build();
+         */
+        when(repository.findByRegistrationId("google")).thenReturn(mockRegistration);
         //uri = mock(String.class);
         uri = "/oauth2/authorization";
         resolver = new CustomizationRequestResolver(repository, uri);
         //String requestUri = "/oauth2/authorization/900783094046-ofbmg9vjjj3s7rlothspmhar185g72il";
         String requestUri = "/oauth2/authorization/google";
         MockHttpServletRequest mockRequest = new MockHttpServletRequest("GET", requestUri);
-        mockRequest.setServletPath("900783094046-ofbmg9vjjj3s7rlothspmhar185g72il.apps.googleusercontent.com");
+        mockRequest.setServletPath(requestUri);
         mockRequest.addParameter("pubkey", "secret");
         req = resolver.resolve(mockRequest);
     }
