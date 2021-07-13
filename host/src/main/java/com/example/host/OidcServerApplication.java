415db7d5bc6cce554218fea40390986a9ba44e58
Daniel Shteinbok <dshteinbok@gmail.com>
2021-07-13 11:09:24 -0400

    added Bean mailToSend that handles the enclave sendig mail out

--- host/src/main/java/com/example/host/OidcServerApplication.java
+++ host/src/main/java/com/example/host/OidcServerApplication.java
@@ -2,11 +2,15 @@ package com.example.host;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonTypeId;
+import com.r3.conclave.host.AttestationParameters;
 import com.r3.conclave.host.EnclaveHost;
 import com.r3.conclave.host.EnclaveLoadException;
+import com.r3.conclave.host.MailCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.config.web.servlet.oauth2.client.OAuth2ClientSecurityMarker;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Hashtable;
+import java.util.concurrent.atomic.AtomicReference;
 
 @SpringBootApplication
 public class OidcServerApplication {
@@ -32,8 +37,22 @@ public class OidcServerApplication {
 		}
 		String enclaveName = "com.example.enclave.VerifierEnclave";
 		EnclaveHost enclave = EnclaveHost.load(enclaveName);
-		enclave.start(null, null);
+		//enclave.start(null, null);
+		System.out.println("Created enclave");
 		return enclave;
+	}
+
+	@Bean
+	public AtomicReference<byte[]> mailToSend() throws EnclaveLoadException {
+	    AtomicReference<byte[]> mailToSend = new AtomicReference<>();
+	    enclave().start(new AttestationParameters.DCAP(), (commands) -> {
+	    	for (MailCommand command : commands) {
+	    		if (command instanceof MailCommand.PostMail) {
+	    			mailToSend.set(((MailCommand.PostMail) command).getEncryptedBytes());
+				}
+			}
+		});
+	    return mailToSend;
 	}
 
 	public static void main(String[] args) {
