3f9b0fc92c4a452baa01bbf5c859c34225b25add
Daniel Shteinbok <dshteinbok@gmail.com>
2021-07-14 12:28:42 -0400

    redid the unit test for CustomizationRequestResolver

--- host/src/test/java/com/example/host/CustomRequestResolverTests.java
+++ host/src/test/java/com/example/host/CustomRequestResolverTests.java
@@ -4,21 +4,58 @@ import com.r3.conclave.shaded.kotlin.text.Charsets;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InOrder;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
+import java.util.function.Consumer;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+/**
+ * Should test that, upon CustomizationRequestResolver.resolve()
+ * the setAuthorizationRequestCustomizer() is called,
+ * THEN the resolve() method of the passed-in DefaultOAuth2AuthorizationRequestResolver
+ * is called.
+ *
+ * Should also verify that the OAuth2AuthorizationRequest produced by
+ * the resolve() method contains the correct attribute ("secret")
+ * and the correct additional parameter ("K7gNU3sdo-OL0wNhqoVWhr3g6s1xYv72ol_pe_Unols",
+ * the hash of "secret")
+ *
+ * NOTE: I have not gotten to testing that last part.
+ * For now, I assume that DefaultOAuth2AuthorizationRequestResolver
+ * works as expected, meaning that if setAuthorizationRequestCustomizer()
+ * is called before resolve(), that whatever Consumer was passed in is applied.
+ *
+ * HOWEVER, the consumer passed in may itself be incorrect,
+ * which would lead to the OAuth2AuthorizationRequest
+ * fields being incorrect.
+ * Since a) the method that produces this consumer is private,
+ * and b) this is all sort of "implementation detail" that should not be tested,
+ * I will not attempt to test this for now.
+ * Furthermore, these tests should not be done in the future the way they are done now.
+ * Instead, the test should just focus on input and output
+ * (i.e. a known MockHttpServletRequest input and an expected output
+ * with expected nonce parameter and attribute).
+ * Currently, changing the internal logic (which should not be tested)
+ * would cause these crappy tests to fail (as they are brittle)
+ */
 public class CustomRequestResolverTests {
     /*
     sample pubkey:
@@ -28,46 +65,43 @@ public class CustomRequestResolverTests {
     K7gNU3sdo-OL0wNhqoVWhr3g6s1xYv72ol_pe_Unols
     */
 
-    private CustomizationRequestResolver resolver;
-    private ClientRegistrationRepository repository;
-    private String uri;
-    OAuth2AuthorizationRequest req;
-
-    @BeforeEach
-    public void setUp() {
-        // DOES NOT WORK!!
-        repository = mock(ClientRegistrationRepository.class);
-        ClientRegistration mockRegistration = mock(ClientRegistration.class);
-        when(mockRegistration.getRegistrationId()).thenReturn("google");
-        when(mockRegistration.getClientAuthenticationMethod()).thenReturn(ClientAuthenticationMethod.CLIENT_SECRET_JWT);
-        when(mockRegistration.getAuthorizationGrantType()).thenReturn(AuthorizationGrantType.AUTHORIZATION_CODE);
-        when(mockRegistration.getScopes()).thenReturn(Collections.singleton(OidcScopes.OPENID));
-        /*
-        ClientRegistration mockRegistration = (new ClientRegistration.Builder())
-                .registrationId("google")
-                .clientId("900783094046-ofbmg9vjjj3s7rlothspmhar185g72il.apps.googleusercontent.com")
-                .clientName("")
-                .clientSecret("")
-                .build();
-         */
-        when(repository.findByRegistrationId("google")).thenReturn(mockRegistration);
-        //uri = mock(String.class);
-        uri = "/oauth2/authorization";
-        resolver = new CustomizationRequestResolver(repository, uri);
-        //String requestUri = "/oauth2/authorization/900783094046-ofbmg9vjjj3s7rlothspmhar185g72il";
-        String requestUri = "/oauth2/authorization/google";
-        MockHttpServletRequest mockRequest = new MockHttpServletRequest("GET", requestUri);
-        mockRequest.setServletPath(requestUri);
-        mockRequest.addParameter("pubkey", "secret");
-        req = resolver.resolve(mockRequest);
-    }
-
+    /**
+     * Test that the resolve() method of the passed-in
+     * DefaultOAuth2AuthorizationRequestResolver is called.
+     */
     @Test
-    public void testInsertsPublicKeyHashToNonce() {
-        assert(req.getAdditionalParameters().get("nonce").equals("K7gNU3sdo-OL0wNhqoVWhr3g6s1xYv72ol_pe_Unols"));
+    public void testResolveCalled() {
+        DefaultOAuth2AuthorizationRequestResolver mockResolver = mock(DefaultOAuth2AuthorizationRequestResolver.class);
+        CustomizationRequestResolver customResolver = new CustomizationRequestResolver(mockResolver);
+
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.addParameter("pubkey", "secret");
+
+        customResolver.resolve(mockHttpServletRequest);
+        verify(mockResolver).resolve(mockHttpServletRequest);
     }
 
-    @Test public void testInsertsPublicKeyToRequestAttribute() {
-        assert(req.getAttribute("nonce").equals("secret"));
+    /**
+     * Test that the setAuthorizationRequestCustomizer() method of the passed-in
+     * DefaultOAuth2AuthorizationRequestResolver is called before
+     * the resolve() method
+     */
+    @Test
+    public void testCustomizerSet() {
+        DefaultOAuth2AuthorizationRequestResolver mockResolver = mock(DefaultOAuth2AuthorizationRequestResolver.class);
+        CustomizationRequestResolver customResolver = new CustomizationRequestResolver(mockResolver);
+
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.addParameter("pubkey", "secret");
+
+        customResolver.resolve(mockHttpServletRequest);
+
+        // make sure stuff happens in order
+        InOrder inOrder = inOrder(mockResolver);
+
+        inOrder.verify(mockResolver).setAuthorizationRequestCustomizer(any());
+        inOrder.verify(mockResolver).resolve(mockHttpServletRequest);
+        inOrder.verifyNoMoreInteractions();
+
     }
 }
