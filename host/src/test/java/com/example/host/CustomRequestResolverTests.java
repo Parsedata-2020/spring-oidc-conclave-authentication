14f8f169f2be64d2309ba5e80a629dc054ea38c7
Daniel Shteinbok <dshteinbok@gmail.com>
2021-07-14 13:54:51 -0400

    added unit test for hashing

--- host/src/test/java/com/example/host/CustomRequestResolverTests.java
+++ host/src/test/java/com/example/host/CustomRequestResolverTests.java
@@ -1,9 +1,18 @@
 package com.example.host;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
 
 import static org.mockito.Mockito.*;
 
@@ -13,28 +22,17 @@ import static org.mockito.Mockito.*;
  * THEN the resolve() method of the passed-in DefaultOAuth2AuthorizationRequestResolver
  * is called.
  *
- * Should also verify that the OAuth2AuthorizationRequest produced by
- * the resolve() method contains the correct attribute ("secret")
- * and the correct additional parameter ("K7gNU3sdo-OL0wNhqoVWhr3g6s1xYv72ol_pe_Unols",
- * the hash of "secret")
+ * Also, verifies that the customizer Consumer passed to setAuthorizationRequestCustomizer()
+ * performs the correct operation; namely, it sets the attribute and nonce parameter to what
+ * is expected.
  *
- * NOTE: I have not gotten to testing that last part.
- * For now, I assume that DefaultOAuth2AuthorizationRequestResolver
- * works as expected, meaning that if setAuthorizationRequestCustomizer()
- * is called before resolve(), that whatever Consumer was passed in is applied.
+ * Together, assuming that the DefaultOAuth2AuthorizationRequestResolver
+ * works as expected, this should mean that the entire class works as expected.
  *
- * HOWEVER, the consumer passed in may itself be incorrect,
- * which would lead to the OAuth2AuthorizationRequest
- * fields being incorrect.
- * Since a) the method that produces this consumer is private,
- * and b) this is all sort of "implementation detail" that should not be tested,
- * I will not attempt to test this for now.
- * Furthermore, these tests should not be done in the future the way they are done now.
- * Instead, the test should just focus on input and output
- * (i.e. a known MockHttpServletRequest input and an expected output
- * with expected nonce parameter and attribute).
- * Currently, changing the internal logic (which should not be tested)
- * would cause these crappy tests to fail (as they are brittle)
+ * NOTE that this test is "brittle"; it relies on an implementation strategy
+ * (the use of setAuthorizationRequestCustomizer) which isn't necessarily the only way
+ * to do this and is not (supposed to be) guaranteed through the "contract" of this class.
+ * This means that a valid, creative implementation in the future may break this test.
  */
 public class CustomRequestResolverTests {
     /*
@@ -45,15 +43,29 @@ public class CustomRequestResolverTests {
     K7gNU3sdo-OL0wNhqoVWhr3g6s1xYv72ol_pe_Unols
     */
 
+    DefaultOAuth2AuthorizationRequestResolver mockResolver;
+    CustomizationRequestResolver customResolver;
+
+    /**
+     * Initialize the mock reslover and our own custom resolver,
+     * which we will use in all subsequent tests
+     */
+    @BeforeEach
+    public void setup() {
+        // the mock default resolver
+        // TODO make this a field
+        mockResolver = mock(DefaultOAuth2AuthorizationRequestResolver.class);
+
+        // our own custom resolver, which uses the mock resolver
+        customResolver = new CustomizationRequestResolver(mockResolver);
+    }
+
     /**
      * Test that the resolve() method of the passed-in
      * DefaultOAuth2AuthorizationRequestResolver is called.
      */
     @Test
     public void testResolveCalled() {
-        DefaultOAuth2AuthorizationRequestResolver mockResolver = mock(DefaultOAuth2AuthorizationRequestResolver.class);
-        CustomizationRequestResolver customResolver = new CustomizationRequestResolver(mockResolver);
-
         MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
         mockHttpServletRequest.addParameter("pubkey", "secret");
 
@@ -68,9 +80,6 @@ public class CustomRequestResolverTests {
      */
     @Test
     public void testCustomizerSet() {
-        DefaultOAuth2AuthorizationRequestResolver mockResolver = mock(DefaultOAuth2AuthorizationRequestResolver.class);
-        CustomizationRequestResolver customResolver = new CustomizationRequestResolver(mockResolver);
-
         MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
         mockHttpServletRequest.addParameter("pubkey", "secret");
 
@@ -82,6 +91,82 @@ public class CustomRequestResolverTests {
         inOrder.verify(mockResolver).setAuthorizationRequestCustomizer(any());
         inOrder.verify(mockResolver).resolve(mockHttpServletRequest);
         inOrder.verifyNoMoreInteractions();
+
+    }
+
+    /**
+     * Test that the returned OAuth2AuthorizationRequest
+     * has its nonce attribute (which will not be sent to the IDP)
+     * set to the public key of the user (in this case, "secret")
+     */
+    @Test
+    public void testReturnedRequestAttribute() {
+        ArgumentCaptor<Consumer<OAuth2AuthorizationRequest.Builder>>
+                customizer = ArgumentCaptor.forClass(Consumer.class);
+
+        // make a test request
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.addParameter("pubkey", "secret");
+
+        customResolver.resolve(mockHttpServletRequest);
+
+        verify(mockResolver).setAuthorizationRequestCustomizer(customizer.capture());
+        OAuth2AuthorizationRequest.Builder builder = TestOAuth2AuthorizationRequests.oidcRequest();
+        customizer.getValue().accept(builder);
+
+        OAuth2AuthorizationRequest builtRequest = builder.build();
+        assert(builtRequest.getAttribute(OidcParameterNames.NONCE).equals("secret"));
+    }
+
+    /**
+     * Test that the returned OAuth2AuthorizationRequest has its "nonce" property
+     * (which will be sent to the IDP) set to "K7gNU3sdo-OL0wNhqoVWhr3g6s1xYv72ol_pe_Unols"
+     * (which is the sha256 hash of "secret")
+     */
+    @Test
+    public void testReturnedRequestParameter() {
+        ArgumentCaptor<Consumer<OAuth2AuthorizationRequest.Builder>>
+                customizer = ArgumentCaptor.forClass(Consumer.class);
+
+        // make a test request
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.addParameter("pubkey", "secret");
+
+        customResolver.resolve(mockHttpServletRequest);
+
+        verify(mockResolver).setAuthorizationRequestCustomizer(customizer.capture());
+        OAuth2AuthorizationRequest.Builder builder = TestOAuth2AuthorizationRequests.oidcRequest();
+        customizer.getValue().accept(builder);
+
+        OAuth2AuthorizationRequest builtRequest = builder.build();
+        assert(builtRequest.getAdditionalParameters().get("nonce")
+                .equals("K7gNU3sdo-OL0wNhqoVWhr3g6s1xYv72ol_pe_Unols"));
+    }
+
+
+    public static final class TestOAuth2AuthorizationRequests {
+
+        private TestOAuth2AuthorizationRequests() {
+        }
+
+        public static OAuth2AuthorizationRequest.Builder request() {
+            String registrationId = "registration-id";
+            String clientId = "client-id";
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put(OAuth2ParameterNames.REGISTRATION_ID, registrationId);
+            // @formatter:off
+            return OAuth2AuthorizationRequest.authorizationCode()
+                    .authorizationUri("https://example.com/login/oauth/authorize")
+                    .clientId(clientId)
+                    .redirectUri("https://example.com/authorize/oauth2/code/registration-id")
+                    .state("state")
+                    .attributes(attributes);
+            // @formatter:on
+        }
+
+        public static OAuth2AuthorizationRequest.Builder oidcRequest() {
+            return request().scope("openid");
+        }
 
     }
 }
