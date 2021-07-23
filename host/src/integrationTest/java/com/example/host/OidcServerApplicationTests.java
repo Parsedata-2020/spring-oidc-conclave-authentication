4d25a2cfe31f5fa1c6e30414e67f10912d13383f
Daniel Shteinbok <dshteinbok@gmail.com>
2021-07-23 10:43:37 -0400

    tried to stub test with wiremock, failed

--- host/src/integrationTest/java/com/example/host/OidcServerApplicationTests.java
+++ host/src/integrationTest/java/com/example/host/OidcServerApplicationTests.java
@@ -1,19 +1,81 @@
 package com.example.host;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 
-@SpringBootTest
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureMockMvc
 class OidcServerApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+    private WireMockServer wireMockServer;
+
+    @BeforeEach
+	public void setup() {
+    	wireMockServer = new WireMockServer(wireMockConfig().port(8090).extensions(CaptureStateTransformer.class));
+    	wireMockServer.start();
+
+    	// Garbage collection go brrrrr
+
+        stubFor(get(urlPathMatching("/oauth/authorize.*"))
+                .willReturn(aResponse()
+                        .withStatus(302)
+                        .withHeader("Location", "http://localhost:8080/login/oauth2/code/my-oauth-client?code=my-acccess-code&state=${state}")
+                        .withTransformers("CaptureStateTransformer")
+                )
+        );
+
+        stubFor(post(urlPathMatching("/oauth/token"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"access_token\":\"my-access-token\"" +
+                                ", \"token_type\":\"Bearer\"" +
+                                ", \"expires_in\":\"3600\"" +
+                                "}")
+                )
+        );
+
+        stubFor(get(urlPathMatching("/userinfo"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"sub\":\"my-user-id\"" +
+                                ",\"name\":\"Mark Hoogenboom\"" +
+                                ", \"email\":\"mark.hoogenboom@example.com\"" +
+                                "}")
+                )
+        );
+
+    }
+
+    /*
+    // this is Junit5--@Rules don't apply!
+    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().port(8090)
+			.extensions(CaptureStateTransformer.class)
+     */
 
     /*
 	@Autowired
 	private MockMvc mockMvc;
      */
 
-    // TODO: check that proper beans are generated
+    // TODO: check that proper beans are generated // set up a Mock OAuth server
+
     //  (is this class really worthy of a unit test?)
 
 	// All of the below should be in integration test folder, not unit test
@@ -22,29 +84,11 @@ class OidcServerApplicationTests {
 	}
 
 	@Test
-	public void authenticatedUserSendingMail() {
+	public void authenticatedUserSendingMail() throws Exception {
 		// user should successfully send mail to the enclave, need some enclave feedback?
         // user sends messages to /user/message
-
-		// TODO: generate a proper id token
-        // String id_token = "";
-
-        final String id_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjFiZjhhODRkM2VjZDc3ZTlmMmFkNWYwNmZm" +
-                "ZDI2MDcwMWRkMDZkOTAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZ" +
-                "S5jb20iLCJhenAiOiI5MDA3ODMwOTQwNDYtb2ZibWc5dmpqajNzN3Jsb3Roc3BtaGFyMTg1ZzcyaWwuY" +
-                "XBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI5MDA3ODMwOTQwNDYtb2ZibWc5dmpqajNzN" +
-                "3Jsb3Roc3BtaGFyMTg1ZzcyaWwuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTMwM" +
-                "DU2MzI1MjAzOTIxMjcyOTgiLCJlbWFpbCI6ImRzaHRlaW5ib2tAZ21haWwuY29tIiwiZW1haWxfdmVya" +
-                "WZpZWQiOnRydWUsImF0X2hhc2giOiI0Qy05aFRSdE1pMk1ub0pNdU9Uakx3Iiwibm9uY2UiOiJURGlYS" +
-                "HVGSE4weTUwWlEtelplMHBZOVFUbjdXSHVQSVZ1Nko1c19KbWVrIiwibmFtZSI6IkRhbmllbCBTaHRla" +
-                "W5ib2siLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EtL0FPaDE0R" +
-                "2pXWDJRU0gtSjFLY01ZSko5U3Jrc09Bd3NxdTYwTlU4elNTSW0xPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6I" +
-                "kRhbmllbCIsImZhbWlseV9uYW1lIjoiU2h0ZWluYm9rIiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2MjYzN" +
-                "Tg0NjYsImV4cCI6MTYyNjM2MjA2Nn0.lEKdWX6d9YrYX6fnuzzl_S-gJF_Pk3-7W3ekbJLZQJnJB8iWW" +
-                "8qAV7j_tcxDmIb9h3H3qAXHmbStuuayJSwnRYEDgF8-oYhXdaYV22tiCL8MbGOLKjZaOzinSENJirrhu" +
-                "BiYvvl4IrMFxHlXl46oPJWhaNbfsFUMT_q7PG6R7BjikajXM9zj1qVe4UP-DNdvXngImgnH_XBkJI5ak" +
-                "wP3lEmJdzU5qD0_spTRliKN2OHWRFCxB3mEY9ctrlBX65Lb1EARrCpQ4Ke" +
-                "XA4M_aEQS3mrg1GzFiVHc2lRhPaMwgkwhpn8Aj4-LuaW4j33R5tO1y2WSTQuT9MKeRyGQmTf09Q";
+        mockMvc.perform((RequestBuilder) get("/user"))
+		.andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status().isOk());
 
 	}
 
